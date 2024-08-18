445fb5b2fdd3312003b3bb75af38f79b0e303352
Your Name <you@example.com>
2024-08-18 09:05:32 +0200

    Make dummy specs more realistic for measurements

--- spec/my_spec.js
+++ spec/my_spec.js
@@ -1,9 +1,12 @@
-fit('works', () => {
-  expect(true).toBe(true);
-})
-
-for (let i = 0; i < 4000; i++) {
-  it('also works', () => {
+for (let i = 0; i < 5000; i++) {
+  fit("works", () => {
+    for (let j = i * 5; j > 0; j--) document.createElement("div");
     expect(true).toBe(true);
-  })
+  });
+}
+
+for (let i = 0; i < 5000; i++) {
+  it("also works", () => {
+    expect(true).toBe(true);
+  });
 }
